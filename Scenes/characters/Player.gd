a179da7afef18a395431f6176007d8ba7bc37dc0
Bruno Lippert <brunohlippert@gmail.com>
2022-05-17 09:11:25 -0300

    new robots

--- Scenes/characters/Player.gd
+++ Scenes/characters/Player.gd
@@ -2,13 +2,26 @@ extends KinematicBody2D
 
 export var SPEED = 250
 var velocity = Vector2()
-
+onready var sprite := $AnimatedSprite
 
 func get_input():
 	var d := Vector2.ZERO
 	
 	d.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
 	d.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
+	
+	if d.y > 0:
+		sprite.play("down")
+	elif d.y < 0:
+		sprite.play("up")
+	elif d.x > 0:
+		sprite.play("right")
+	elif d.x < 0:
+		sprite.play("left")
+	else:
+		sprite.stop()
+		sprite.frame = 0
+		
 	return d
 		
 func _physics_process(delta):
